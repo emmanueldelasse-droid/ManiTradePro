c099228c47b423c56c24f623c38c559f082ff686
emmanueldelasse-droid <emmanueldelasse@hotmail.fr>
2026-04-06 23:02:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "manitradepro-v5.2";
+const CACHE_VERSION = "manitradepro-v5.3";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
